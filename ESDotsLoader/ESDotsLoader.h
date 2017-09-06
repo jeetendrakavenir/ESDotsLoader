1ea2cf008d072c5ad8b14fca16599e16813c383d
Jeetendra <jeetendrak.avenir@emaar.ae>
2017-09-06 08:28:12 +0400

    Initial Commit

---
+++ ESDotsLoader/ESDotsLoader.h
@@ -0,0 +1,19 @@
+//
+//  ESDotsLoader.h
+//  ESDotsLoader
+//
+//  Created by Jeetendra on 9/6/17.
+//  Copyright © 2017 Jeetendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESDotsLoader.
+FOUNDATION_EXPORT double ESDotsLoaderVersionNumber;
+
+//! Project version string for ESDotsLoader.
+FOUNDATION_EXPORT const unsigned char ESDotsLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESDotsLoader/PublicHeader.h>
+
+
